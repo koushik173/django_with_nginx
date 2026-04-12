7f6af50f6ba49bad99832f4e84e0b44cf559ccfd
koushik173 <koushikroy298@gmail.com>
2026-04-12 15:29:38 +0600

    hk just inital

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from my-django-with-nginx!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ mydjangoin/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MydjangoinConfig(AppConfig):
+    name = 'mydjangoin'
